7ae333bee9c3cd66ef7a5ce8842b3e2c51bcc245
moomurrs <59849596+moomurrs@users.noreply.github.com>
2020-02-11 18:37:18 -0500

    Finishes assignment E05

--- Scripts/Ship.gd
+++ Scripts/Ship.gd
@@ -5,4 +5,12 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	pass
+	if Input.is_action_pressed("ui_left"):
+		position.x = position.x - 10
+		print("pressed left")
+	if Input.is_action_pressed("ui_right"):
+		position.x += 10
+	if Input.is_action_pressed("ui_down"):
+		position.y += 10
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 10
